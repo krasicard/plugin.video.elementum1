50bed563044af62dc0e6eb77f935f7ef3f1e60c5
Dim0N <dim0000n@gmail.com>
2021-04-26 21:51:46 -0700

    Fix list row output

--- resources/web/react-new/elementum-web/src/torrents-list/torrent/index.tsx
+++ resources/web/react-new/elementum-web/src/torrents-list/torrent/index.tsx
@@ -57,17 +57,17 @@ const TorrentListItem = ({ torrent, onTorrentSelected }: ITorrentListItemProps):
           <Label color={statusLabelColor}>{torrent.status}</Label>
         </Table.Cell>
         <Table.Cell>
-          <StatisticGroup widths="2" size="mini">
+          <StatisticGroup widths="3" size="mini">
             <Statistic value={torrent.ratio.toFixed(2)} label="Seed ratio" />
             <Statistic value={torrent.time_ratio.toFixed(2)} label="Time ratio" />
-            <Statistic value={torrent.seed_time} label="Seed time" />
+            <Statistic value={`${torrent.seed_time}s`} label="Seed time" />
           </StatisticGroup>
         </Table.Cell>
         <Table.Cell>
           <Label>{torrent.size}</Label>
         </Table.Cell>
         <Table.Cell>
-          <StatisticGroup widths="1" size="mini">
+          <StatisticGroup widths="2" size="mini">
             <Statistic>
               <StatisticValue>
                 <Icon name="arrow down" size="small" />
@@ -83,13 +83,13 @@ const TorrentListItem = ({ torrent, onTorrentSelected }: ITorrentListItemProps):
           </StatisticGroup>
         </Table.Cell>
         <Table.Cell>
-          <StatisticGroup widths="1" size="mini">
+          <StatisticGroup widths="2" size="mini">
             <Statistic value={torrent.seeders} label="Active" />
             <Statistic value={torrent.seeders_total} label="Total" />
           </StatisticGroup>
         </Table.Cell>
         <Table.Cell>
-          <StatisticGroup widths="1" size="mini">
+          <StatisticGroup widths="2" size="mini">
             <Statistic value={torrent.peers} label="Active" />
             <Statistic value={torrent.peers_total} label="Total" />
           </StatisticGroup>
