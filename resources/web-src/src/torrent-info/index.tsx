8eeb096e33fdb1a1fa96f7a6b8ba644cfca24217
Dim0N <dim0000n@gmail.com>
2021-05-04 23:15:19 -0700

    Add pieces and files tabs

--- resources/web-src/src/torrent-info/index.tsx
+++ resources/web-src/src/torrent-info/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, GridColumn, GridRow, Tab } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Grid, GridColumn, GridRow, List, Tab } from 'semantic-ui-react';
 import { ITorrentView } from '../dataStructure';
 
 interface ITorrentInfoItemProps {
@@ -8,6 +8,32 @@ interface ITorrentInfoItemProps {
 }
 
 const TorrentInfo = ({ torrent }: ITorrentInfoItemProps): JSX.Element => {
+  const [filesList, setFilesList] = useState<string[]>([]);
+  const [piecesList, setPiecesList] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const getInfo = async () => {
+      const regexp = new RegExp(`${torrent.id}.+?Files.+?:\\s+(?<files>.+?)\\n\\n.+?Pieces:\\s+(?<pieces>.+?)\\n\\n\\n`, 'gs');
+      const response = await fetch('/info');
+      const info = await response.text();
+      const match = regexp.exec(info);
+      if (!match || !match.groups) return;
+
+      const { files, pieces } = match.groups;
+      setFilesList(files.split('\n').map((f) => f.trim()));
+      setPiecesList(pieces);
+
+      setLoading(false);
+    };
+
+    void getInfo();
+    const intervalHandle = setInterval(() => void getInfo(), 5000);
+    return () => clearInterval(intervalHandle);
+  }, [torrent.name]);
+
   const panes = [
     {
       menuItem: { key: 'general', content: 'General' },
@@ -22,6 +48,34 @@ const TorrentInfo = ({ torrent }: ITorrentInfoItemProps): JSX.Element => {
                 <div>
                   <b>Size:</b> {torrent.size}
                 </div>
+              </GridColumn>
+            </GridRow>
+          </Grid>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: { key: 'files', content: 'Files' },
+      render: () => (
+        <Tab.Pane loading={loading}>
+          <Grid>
+            <GridRow>
+              <GridColumn>
+                <List items={filesList} />
+              </GridColumn>
+            </GridRow>
+          </Grid>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: { key: 'pieces', content: 'Pieces' },
+      render: () => (
+        <Tab.Pane loading={loading}>
+          <Grid>
+            <GridRow>
+              <GridColumn>
+                <div>{piecesList}</div>
               </GridColumn>
             </GridRow>
           </Grid>
